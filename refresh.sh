846c779b2cc2f6738b6abfbf087ae82b0d3f3cdd
William Wang <wyw6@cornell.edu>
2022-06-12 22:57:31 -0400

    [Refresh] 06/12/2022

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cp /usr/local/bin/* src/
-
+git add -u
+DATE=$(date +'%m/%d/%Y')
+git commit -m "[Refresh] ${DATE}"
+git push
--- src/update_chezmoi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Update the dotfiles that are listed in the DOTFILES env variable
-for FILE in $(cat "$DOTFILES") ; do
-	echo "Adding ${FILE}"
-	chezmoi add $FILE
-done
-
-DATE=$(date +'%m/%d/%Y')
